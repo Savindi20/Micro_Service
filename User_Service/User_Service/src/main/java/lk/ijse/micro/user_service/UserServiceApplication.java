d745a877293f06a1d7f86596dbe8f66d2700c8fb
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-11-27 12:09:27 +0530

    project updated

--- User_Service/User_Service/src/main/java/lk/ijse/micro/user_service/UserServiceApplication.java
+++ User_Service/User_Service/src/main/java/lk/ijse/micro/user_service/UserServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -19,6 +18,5 @@ public class UserServiceApplication {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
-
 
 }
