6001ed415505ca1ca3e3e853870c67849ee51579
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-11-30 12:01:37 +0530

    user_service project updated

--- User_Service/User_Service/src/main/java/lk/ijse/micro/user_service/controller/UserController.java
+++ User_Service/User_Service/src/main/java/lk/ijse/micro/user_service/controller/UserController.java
@@ -1,27 +1,15 @@
 package lk.ijse.micro.user_service.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("api/v1/user")
-@CrossOrigin
+@RequestMapping("/user")
 public class UserController {
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     @GetMapping("/save")
     public String saveUser(){
-        return restTemplate.getForObject("http://localhost:8082/api/v1/order/get",String.class);
-    }
-
-    @GetMapping("/test")
-    public String test(){
-        return "I'm completed";
+        return "saved user!";
     }
 }
